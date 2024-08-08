cf00cf98fd01a612431f9f0803e57d161d16f207
AnotherDev <joseluismanco37@gmail.com>
2024-08-08 16:16:09 -0500

    chore: change sepolia for base-sepolia

--- packages/front-end/config.ts
+++ packages/front-end/config.ts
@@ -1,5 +1,5 @@
 import { AlchemyAccountsUIConfig, createConfig } from "@account-kit/react";
-import { sepolia } from "@account-kit/infra";
+import { baseSepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
 const uiConfig: AlchemyAccountsUIConfig = {
@@ -14,7 +14,7 @@ export const config = createConfig({
   // if you don't want to leak api keys, you can proxy to a backend and set the rpcUrl instead here
   // get this from the app config you create at https://dashboard.alchemy.com/accounts
   apiKey: "Sba5LfgDpvccNFsfyk7HEhmRDkEaVXSF",
-  chain: sepolia,
+  chain: baseSepolia,
   ssr: true, // set to false if you're not using server-side rendering
 }, uiConfig);
 
