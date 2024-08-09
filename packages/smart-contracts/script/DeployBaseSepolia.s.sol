f654b3afb8797992ad8e2549b3a5364327bfa862
AnotherDev <joseluismanco37@gmail.com>
2024-08-08 23:34:37 -0500

    feat: testing hypercerts

--- packages/smart-contracts/script/DeployBaseSepolia.s.sol
+++ packages/smart-contracts/script/DeployBaseSepolia.s.sol
@@ -16,35 +16,35 @@ contract DeployBaseSepolia is Script {
 
     function run() public {
         vm.startBroadcast();
-        IEAS eas = IEAS(0x4200000000000000000000000000000000000021);
+        // IEAS eas = IEAS(0x4200000000000000000000000000000000000021);
         RealizeIT realizeIT = new RealizeIT(
             IWorldID(0x42FF98C4E85212a5D31358ACbFe76a621b50fC02),
             "app_staging_4989e6a8b385ae6116fb36aeae08c250",
             "realizeit",
             IHypercertToken(0xC2d179166bc9dbB00A03686a5b17eCe2224c2704)
         );
-        NOTUSDC notusdc = new NOTUSDC();
-        RegistrationResolver registrationResolver = new RegistrationResolver(
-            eas,
-            realizeIT
-        );
-        Points points = new Points(msg.sender);
-        CheckoutResolver checkoutResolver = new CheckoutResolver(
-            eas,
-            realizeIT
-        );
-
+        // NOTUSDC notusdc = new NOTUSDC();
+        // RegistrationResolver registrationResolver = new RegistrationResolver(
+        //     eas,
+        //     realizeIT
+        // );
+        // Points points = new Points(msg.sender);
+        // CheckoutResolver checkoutResolver = new CheckoutResolver(
+        //     eas,
+        //     realizeIT
+        // );
+        //
         console.log("realizeIT deployed at: ", address(realizeIT));
-        console.log("NOTUSDC deployed at: ", address(notusdc));
-        console.log(
-            "registrationResolver deployed at: ",
-            address(registrationResolver)
-        );
-        console.log("points deployed at: ", address(points));
-        console.log(
-            "checkoutResolver deployed at: ",
-            address(checkoutResolver)
-        );
+        // console.log("NOTUSDC deployed at: ", address(notusdc));
+        // console.log(
+        //     "registrationResolver deployed at: ",
+        //     address(registrationResolver)
+        // );
+        // console.log("points deployed at: ", address(points));
+        // console.log(
+        //     "checkoutResolver deployed at: ",
+        //     address(checkoutResolver)
+        // );
 
         vm.stopBroadcast();
     }
