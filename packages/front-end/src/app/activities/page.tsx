3543cbcae43bc4a433131d96e1ea5137b2f21097
simon0820s <simon0820s@gmail.com>
2024-08-09 23:53:18 -0500

    feat(styles): fix styles issues

--- packages/front-end/src/app/activities/page.tsx
+++ packages/front-end/src/app/activities/page.tsx
@@ -4,17 +4,35 @@ import React from "react";
 
 export default function Activities() {
   return (
-    <div className="flex flex-col gap-2">
-      {activities.map((activity) => (
-        <ActivityCard
-          key={activity.name}
-          icon={activity.icon}
-          name={activity.name}
-          stars={activity.stars}
-          usdc={activity.usdc}
-          date={activity.date}
-        />
-      ))}
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <p className="text-textSoftGray text-xs">Organized Activities</p>
+        {activities.organized.map((activity) => (
+          <ActivityCard
+            key={activity.name}
+            icon={activity.icon}
+            name={activity.name}
+            stars={activity.stars}
+            usdc={activity.usdc}
+            date={activity.date}
+            opaque
+          />
+        ))}
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-textSoftGray text-xs">More Activities</p>
+        {activities.more.map((activity) => (
+          <ActivityCard
+            key={activity.name}
+            icon={activity.icon}
+            name={activity.name}
+            stars={activity.stars}
+            usdc={activity.usdc}
+            date={activity.date}
+            opaque
+          />
+        ))}
+      </div>
     </div>
   );
 }
