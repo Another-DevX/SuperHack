403605b279b09b285c0e5de113668ee41f597f38
AnotherDev <joseluismanco37@gmail.com>
2024-08-10 18:00:23 -0500

    feat: add support to attestations, feat: add support for subgraph

--- packages/front-end/src/constants/index.ts
+++ packages/front-end/src/constants/index.ts
@@ -2,9 +2,10 @@ import { createPublicClient, erc20Abi, http } from "viem";
 import { baseSepolia } from "viem/chains";
 import RealiezeITContractAbi from "./RealizeIT.abi.json";
 export const REALIZE_IT_CONTRACT_ADDRESS =
-  "0xf4401497511738b7dafd7063c6162ac0d7cf5982";
+  "0x79f0c6E889159BA87A7DafcFC40DDcbdBef9508B";
 export const REALIZE_IT_CONTRACT_ABI = RealiezeITContractAbi;
-export const TOKEN_CONTRACT_ADDRESS = "0xA92E03E4bd369997C8B030dAE854d57b39212E5C"
+export const TOKEN_CONTRACT_ADDRESS =
+  "0xA92E03E4bd369997C8B030dAE854d57b39212E5C";
 export const publicClient = createPublicClient({
   chain: baseSepolia,
   transport: http(),
---
+++ packages/front-end/src/hooks/useGetActivities.tsx
@@ -0,0 +1,21 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+
+function useGetActivities() {
+  const GET_CAMPAIGNS = gql`
+    query GetLocations {
+      campaignCreateds() {
+            id
+    host
+    pricePool
+    maxQuota
+    onlyVerified
+    hypercertID
+    }
+    }
+  `;
+
+  return useQuery(GET_CAMPAIGNS);
+}
+
+export default useGetActivities;
